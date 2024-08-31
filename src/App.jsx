d5bce51bf1ecd408f1bfa1fc97f438d72793d26e
Lilia Levchenko <liliya.benatti@gmail.com>
2024-08-31 20:11:20 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,20 @@ import SecondPage from 'pages/SecondPage/SecondPage';
 import HalfPage from 'pages/HalfPage/HalfPage';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
 
-
 const test = import.meta.env.VITE_API_TEST;
 
 function App() {
   console.log(test);
   return (
-    <AppWrapper>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route path="/first" element={<HomePage />} />
-          <Route path="/second" element={<SecondPage />}>
-            <Route path=":half" element={<HalfPage />} />
-          </Route>
-          <Route path="*" element={<ErrorPage />} />
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route path="/first" element={<HomePage />} />
+        <Route path="/second" element={<SecondPage />}>
+          <Route path=":half" element={<HalfPage />} />
         </Route>
-      </Routes>
-    </AppWrapper>
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
   );
 }
 export default App;
