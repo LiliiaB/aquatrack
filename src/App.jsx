c7702e5e2f8e7b457986cc928023d6d956dff32e
Lilia Levchenko <liliya.benatti@gmail.com>
2024-09-14 22:20:46 +0200

    nav bar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
-import SharedLayout from 'components/SharedLayout/SharedLayout';
 import HomePage from 'pages/HomePage/FirstPage';
-import SecondPage from 'pages/SecondPage/SecondPage';
-import HalfPage from 'pages/HalfPage/HalfPage';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
-
-const test = import.meta.env.VITE_API_TEST;
+import MainPage from './pages/MainPage/MainPage';
+import SignInPage from './pages/SignInPage/SignInPage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
 
 function App() {
-  console.log(test);
   return (
     <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route path="/first" element={<HomePage />} />
-        <Route path="/second" element={<SecondPage />}>
-          <Route path=":half" element={<HalfPage />} />
-        </Route>
-        <Route path="*" element={<ErrorPage />} />
-      </Route>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/signIn" element={<SignInPage />} />
+      <Route path="/signUp" element={<SignUpPage />} />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 }
